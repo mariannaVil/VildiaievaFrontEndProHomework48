a6c0cccd5464882372c5bb70971bba503ce5fe05
mariannaVil <mariannavild@gmail.com>
2024-01-22 17:06:18 +0200

    Upgrade Congratulation App from the past HW

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import MainContainer from './pages/MainContainer';
 import ModalForm from './components/ModalForm';
 import Card from './components/Card';
-import { creativeCards } from './constants.js';
+import { creativeCards } from './api';
 
 const App = () => {
   const [cards, setCards] = useState([]);
   const [selectedCard, setSelectedCard] = useState(null); 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [formInput, setFormInput] = useState({ title: '', message: '' });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        setLoading(true);
+        const response = await creativeCards.get();
+        setCards(response.data);
+        setError(null);
+      } catch (error) {
+        setError('Error fetching cards');
+        console.error('Error fetching cards:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+  
+    fetchCards();
+  }, []);
 
   const handleGenerateClick = () => {
-    const randomIndex = Math.floor(Math.random() * creativeCards.length);
-    const randomCard = creativeCards[randomIndex];
+    const randomIndex = Math.floor(Math.random() * cards.length);
+    const randomCard = cards[randomIndex];
     setSelectedCard(randomCard);
   };
-
+  
   const handleCreateClick = () => {
     setIsModalOpen(true);
   };
@@ -26,11 +46,17 @@ const App = () => {
     setIsModalOpen(false);
   };
 
-  const handleFormSubmit = () => {
-    setCards([...cards, formInput]);
-    setIsModalOpen(false);
+  const handleFormSubmit = async () => {
+    try {
+      await creativeCards.post({ ...formInput, imgURL: formInput.url });
+      const updatedCards = await creativeCards.get();
+      setCards(updatedCards.data);
+      setIsModalOpen(false);
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
   };
-
+  
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormInput((prevInput) => ({
@@ -38,13 +64,27 @@ const App = () => {
       [name]: value,
     }));
   };
-
+  
+  const handleDeleteClick = async (id) => {
+    try {
+      await creativeCards.delete(id);
+      console.log('Deleted successfully');
+  
+      const updatedCards = await creativeCards.get();
+      setCards(updatedCards.data);
+    } catch (error) {
+      console.error('Error deleting card:', error);
+    }
+  };
+  
   return (
     <>
       <Header />
       <MainContainer
         handleGenerateClick={handleGenerateClick}
         handleCreateClick={handleCreateClick}
+        handleDeleteClick={handleDeleteClick}
+        cards={cards}
       />
       {isModalOpen && (
         <ModalForm
@@ -53,9 +93,14 @@ const App = () => {
           handleInputChange={handleInputChange}
         />
       )}
-      {selectedCard && <Card title={selectedCard.title} message={selectedCard.message} image={selectedCard.image} />}
+      {selectedCard && <Card id={selectedCard.id} title={selectedCard.title} message={selectedCard.message} imgURL={selectedCard.imgURL} />}
       {cards.map((card) => (
-        <Card title={card.title} message={card.message} image={card.image} 
+        <Card id={card.id} 
+          title={card.title} 
+          message={card.message} 
+          imgURL={card.imgURL} 
+          handleDeleteClick={() => handleDeleteClick(card.id)}
+          
         />
       ))}
     </>
--- src/components/ModalForm.jsx
+++ src/components/ModalForm.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const ModalForm = ({ handleSubmit, handleCancel, handleInputChange }) => {
     return (
         <div className='modal-wrapper'>
-        <div className='modal'>
+          <div className='modal'>
             <input type='text' className='title-input' placeholder='Title' name='title' onChange={handleInputChange} />
             <input type='text' className='message-input' placeholder='Message' name='message' onChange={handleInputChange} />
-            <input type='text' className='url-input' placeholder='Url' name='url' onChange={handleInputChange} />
+            <input type='url' className='url-input' placeholder='Url' name='url' onChange={handleInputChange} />
             <button type='submit' className='send-button' onClick={handleSubmit}>Send</button>
             <button type='button' className='cancel-button' onClick={handleCancel}>Cancel</button>
-        </div>
+          </div>
     </div>
     )  
 }
